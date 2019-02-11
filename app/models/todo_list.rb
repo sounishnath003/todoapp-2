16558dd924a1ecf2087ab1d4adbb8d5c2064de38
sounishnath003 <flock.sinasini@gmail.com>
2019-02-12 01:52:00 +0530

    search implement

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,7 @@
 class TodoList < ApplicationRecord
-  has_many :todo_items
+  validates :title, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}
+
+  validates :description, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 5, maximum:50}
+
+  has_many :todo_items, dependent: :destroy
 end
